e410a794fe3599ba79fd00adc2b7baea969d7142
reijomo <95440863+reijomo@users.noreply.github.com>
2025-07-18 10:19:25 +0300

    button class cleaning

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,66 +1,17 @@
+import buttonColors from "../helpers/tailwindClasses"
+
 const Button = ({ text, type, width, height, color, handleClick, disabled, loading }) => {
-  let background = 'bg-blue-700'
-  let hover = 'hover:bg-blue-800'
-  let focus = 'focus:ring-blue-300'
-  let darkBackground = 'dark:bg-blue-600'
-  let darkHover = 'dark:hover:bg-blue-700'
-  let darkFocus = 'dark:focus:ring-blue-800'
-  let textColor = 'text-white'
-  let disabledColor = 'bg-blue-800'
- 
-  if (color === 'blue') {
-    background = 'bg-blue-700'
-    hover = 'hover:bg-blue-800'
-    focus = 'focus:ring-blue-300'
-    darkBackground = 'dark:bg-blue-600'
-    darkHover = 'dark:hover:bg-blue-700'
-    darkFocus = 'dark:focus:ring-blue-800'
-    textColor = 'text-white'
-    disabledColor = 'bg-blue-800'
-  }
 
-  if (color === 'gold') {
-    background = 'bg-yellow-600'
-    hover = 'hover:bg-yellow-700'
-    focus = 'focus:ring-yellow-300'
-    darkBackground = 'dark:bg-yellow-500'
-    darkHover = 'dark:hover:bg-yellow-600'
-    darkFocus = 'dark:focus:ring-yellow-800'
-    textColor = 'text-white'
-    disabledColor = 'bg-yellow-800'
-  }
-
-  if (color === 'red') {
-    background = 'bg-red-600' 
-    hover = 'hover:bg-red-700' 
-    focus = 'focus:ring-red-300' 
-    darkBackground = 'dark:bg-red-500' 
-    darkHover = 'dark:hover:bg-red-600' 
-    darkFocus = 'dark:focus:ring-red-700' 
-    textColor = 'text-white'
-    disabledColor = 'bg-red-700'
-  }
-
-  if (color === 'gray') {
-    background = 'bg-gray-300'
-    hover = 'hover:bg-gray-400'
-    focus = 'focus:ring-gray-300' 
-    darkBackground = 'dark:bg-gray-400' 
-    darkHover = 'dark:hover:bg-gray-500' 
-    darkFocus = 'dark:focus:ring-gray-600' 
-    textColor = 'text-gray-700'
-  }
-
-  if (color === 'orange') {
-    background = 'bg-orange-400' 
-    hover = 'hover:bg-orange-500' 
-    focus = 'focus:ring-orange-300' 
-    darkBackground = 'dark:bg-orange-600' 
-    darkHover = 'dark:hover:bg-orange-700'
-    darkFocus = 'dark:focus:ring-orange-800' 
-    textColor = 'text-white' 
-    disabledColor = 'bg-orange-500'
-  }
+  const {
+    background,
+    hover,
+    focus,
+    darkBackground,
+    darkHover,
+    darkFocus,
+    textColor,
+    disabledColor,
+  } = buttonColors[color] || buttonColors.blue
 
   if (loading) { return <LoadingButton width={width} height={height} color={color} /> }
 
